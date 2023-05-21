1c08de7b81520e09f4422c3663e9e738b0bc5206
Rehan <darrehanrasool@gmail.com>
2023-05-21 18:40:22 +0530

    Dar Rehan Rasool
    sycomusiclover

---
+++ resources/views/Admin/Body/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>document.write(new Date().getFullYear())</script> © Dar Rehan Rasool.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-end d-none d-sm-block">
+                                    Developer  <i class="mdi mdi-circle text-danger"></i> Dar Rehan Rasool
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                w
