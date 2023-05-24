20a0da322bee76207293c9e9b068ff642bb4281a
Rehan <darrehanrasool@gmail.com>
2023-05-24 23:06:46 +0530

    Dar Rehan Rasool
    sycomusiclover

--- app/Http/Controllers/Admincontroller.php
+++ app/Http/Controllers/Admincontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;  // this url is from Authenticationsessioncontroller
-
+use Illuminate\Http\RedirectResponse; // Also use this url link
 class Admincontroller extends Controller
 {
     // Here we are going to write public funtions for route
